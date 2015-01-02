f0c7e9ae7ee156ddce2ab5c715b0f281deb3e8b8
Stephen Coogan <coogie@users.noreply.github.com>
2015-01-02 19:00:11 +0000

    Fix reference to Gravatar img tag

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -8,7 +8,7 @@ function renderDeliciousLinks(items) {
 }
 
 jQuery(function(){
-    var gravatarImage = jQuery('img.gravatar'),
+    var gravatarImage = jQuery('img.hero__gravatar'),
         email         = jQuery(gravatarImage).data('gravatar');
     if (email) {
         jQuery(gravatarImage).attr({src: 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email) + '?s=250'}).removeAttr('data-gravatar');
